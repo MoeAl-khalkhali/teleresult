40b8b966d5d5239cf740bed62e5f5b6279e9b069
s3749653@student.rmit.edu.au <s3749653@student.rmit.edu.au>
2022-08-04 14:19:38 +1000

    added web controller

--- src/main/java/com/teleresult/api/web/OrderController.java
+++ src/main/java/com/teleresult/api/web/OrderController.java
@@ -4,24 +4,28 @@ import com.teleresult.api.model.Order;
 import com.teleresult.api.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/orders")
 public class OrderController {
     @Autowired
     private OrderRepository orderRepository;
 
 
     @GetMapping("/{id}")
-    public ResponseEntity<Order> getOrderById(@PathVariable(value = "id") Long id)
-            throws Exception {
-        Order user =
-                orderRepository
-                        .findById(id)
-                        .orElseThrow(() -> new Exception("Order not found on :: " + id));
-        return ResponseEntity.ok().body(user);
+    public ResponseEntity<?> getOrderById(@PathVariable(value = "id") Long id){
+            try{
+            Order user =
+                    orderRepository
+                            .findById(id)
+                            .orElseThrow(() -> new Exception("Order not found on :: " + id));
+            return ResponseEntity.ok().body(user);
+            }  catch (Exception e)
+        {
+            return ResponseEntity.ok().body("Order " + id + "does not exist");
+        }
     }
     @PostMapping()
     public ResponseEntity<?> createdNewOrder(@RequestBody Order order) {
