62e5cd2742dbfdd86ade98f721d39fcc9bdad1bb
mouhannada <mouhannad_mo@hotmail.com>
2022-08-05 12:37:06 +1000

    added request

--- src/test/java/com/teleresult/api/web/OrderControllerTest.java
+++ src/test/java/com/teleresult/api/web/OrderControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.sql.Date;
 import java.util.HashMap;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -43,11 +44,22 @@ public class OrderControllerTest {
                 .andExpect(status().isOk()).andExpect(content().string("Order 1231432 does not exist"));
     }
     @Test
-    public void getOrder_shouldReturnHTTPStatus200_OrderAlreadyExists() throws Exception {
+    public void createOrder_shouldReturnHTTPStatus200_OrderAlreadyExists() throws Exception {
         HashMap<String, Object> ob = new HashMap<>();
         ob.put("id", "1");
         mvc.perform(MockMvcRequestBuilders.post("/orders").content(new JSONObject(ob).toJSONString())
                 .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk()).andExpect(content().string("Order ID already exists"));
+    }
+    @Test
+    public void createOrder_shouldReturnHTTPStatus$00_whenMissingParameters() throws Exception {
+        HashMap<String, Object> ob = new HashMap<>();
+        ob.put("id", "1");
+        ob.put("date", new Date(3213232));
+        ob.put("customer", "customer-93");
+        ob.put("type", "iPhone14");
+        mvc.perform(MockMvcRequestBuilders.post("/orders").content(new JSONObject(ob).toJSONString())
+                        .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
     }
 }
